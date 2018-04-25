b1c07c1f9f8354809110c99986b61aa1e0e96bde
cryosx <cyruswu.email@gmail.com>
2018-04-25 13:50:38 -1000

    basic structure of the db files implemented

---
+++ db/articles.js
@@ -0,0 +1,14 @@
+module.exports = (function() {
+  let articles = [];
+  return {
+    get: getArticles,
+    create: createArticles,
+    update: updateArticles,
+    delete: deleteArticles
+  };
+
+  function getArticles() {}
+  function createArticles() {}
+  function updateArticles() {}
+  function deleteArticles() {}
+})();
---
+++ db/products.js
@@ -0,0 +1,14 @@
+module.exports = (function() {
+  let products = [];
+  return {
+    get: getProduct,
+    create: createProduct,
+    update: updateProduct,
+    delete: deleteProduct
+  };
+
+  function getProduct() {}
+  function createProduct() {}
+  function updateProduct() {}
+  function deleteProduct() {}
+})();
