d549d71a95fa21fd97ac2f4dd6b367e878a4cbf1
cryosx <cyruswu.email@gmail.com>
2018-04-25 13:59:57 -1000

    handlebars integration

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const server = express();
+const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars');
 
 const articlesRoutes = require('./routes/articles.js');
 const productsRoutes = require('./routes/products.js');
 
 const PORT = process.env.PORT || 3000;
+
+server.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }));
+server.set('view engine', '.hbs');
 
 server.use(express.static('public'));
 
