4e41617cee8d351545598d174d0da9df0122ec85
cryosx <cyruswu.email@gmail.com>
2018-04-25 14:00:16 -1000

    added extra method, getAll

--- db/articles.js
+++ db/articles.js
@@ -2,12 +2,14 @@ module.exports = (function() {
   let articles = [];
   return {
     get: getArticles,
+    getAll: getAllArticles,
     create: createArticles,
     update: updateArticles,
     delete: deleteArticles
   };
 
   function getArticles() {}
+  function getAllArticles() {}
   function createArticles() {}
   function updateArticles() {}
   function deleteArticles() {}
--- db/products.js
+++ db/products.js
@@ -2,12 +2,13 @@ module.exports = (function() {
   let products = [];
   return {
     get: getProduct,
+    getAll: getAllProducts,
     create: createProduct,
     update: updateProduct,
     delete: deleteProduct
   };
-
   function getProduct() {}
+  function getAllProducts() {}
   function createProduct() {}
   function updateProduct() {}
   function deleteProduct() {}
