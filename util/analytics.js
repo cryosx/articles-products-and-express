781ed9d9a489abcf028aeaf9b2c7914874f11c6e
cryosx <cyruswu.email@gmail.com>
2018-04-27 11:33:22 -1000

    analytics implemented

---
+++ util/analytics.js
@@ -0,0 +1,25 @@
+module.exports = (function() {
+  const fs = require('fs');
+  const path = require('path');
+
+  return function analyticsTracker(req, res, next) {
+    let date = new Date();
+
+    let year = date.getFullYear();
+    let month = `${date.getMonth()}`.padStart(2, 0);
+    let day = `${date.getDate()}`.padStart(2, 0);
+    let dateString = `${year}-${month}-${day}`;
+
+    let hours = date.getHours();
+    let minutes = `${date.getMinutes()}`.padStart(2, 0);
+    let seconds = `${date.getSeconds()}`.padStart(2, 0);
+    let timeString = `${hours}:${minutes}:${seconds}`;
+
+    let data = `[${dateString} ${timeString}] ${req.method} ${req.url} `;
+
+    fs.appendFile(`logs/${dateString}.log`, `${data}\r\n`, function(err) {
+      if (err) throw err;
+    });
+    return next();
+  };
+})();
